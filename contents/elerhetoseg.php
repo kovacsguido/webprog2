403329ead4f5514fe02c5d4592ecc49f00f59380
Kovacs Guido <kovacs.guido@gmail.com>
2020-12-03 08:26:27 +0100

    Added the content of the Elerhetosegek page

---
+++ contents/elerhetoseg.php
@@ -0,0 +1,19 @@
+<h1>Elérhetőségek</h1>
+<p class="lead">Figyelem, ez a site egy egyetemi beadandó feladat keretében készült, nem valódi cégről szól!</p>
+<br>
+<table class="table table-bordered">
+    <tbody>
+    <tr>
+        <th scope="row">Cím:</th>
+        <td>1234 Budapest, Diploma u. 3.</td>
+    </tr>
+    <tr>
+        <th scope="row">Telefon:</th>
+        <td><a href="tel:+36155512345">+36 1 555-1234-5</a></td>
+    </tr>
+    <tr>
+        <th scope="row">E-mail</th>
+        <td><a href="mailto:it-dev@example.com">it-dev@example.com</a></td>
+    </tr>
+    </tbody>
+</table>
